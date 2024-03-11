c24f3d2ac5493915f8d91cb2182b659d76656624
EsM3R <105605335+EsM3R@users.noreply.github.com>
2024-03-11 12:25:46 +0300

    Initial Commit

---
+++ HIkeApp/HIkeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIkeAppApp.swift
+//  HIkeApp
+//
+//  Created by Mehmet Vural on 11.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct HIkeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
